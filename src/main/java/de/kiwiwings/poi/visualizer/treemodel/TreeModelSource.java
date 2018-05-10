55cfe5dd94dc924c10d710a8383d85df689054cc
kiwiwings <andreas.beeker@gmx.de>
2018-05-10 18:30:45 +0200

    add license header

--- src/main/java/de/kiwiwings/poi/visualizer/treemodel/TreeModelSource.java
+++
@@ -1,7 +0,0 @@
-package de.kiwiwings.poi.visualizer.treemodel;
-
-import javafx.scene.control.TreeItem;
-
-public interface TreeModelSource {
-    void load(TreeItem<TreeModelEntry> parent, Object source) throws TreeModelLoadException;
-}
